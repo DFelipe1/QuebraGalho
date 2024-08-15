49bfbe0ae6b89ee7a581b9ae68fd96a5b2e54a6f
David Felipe <felipe30.10@outlook.com>
2024-08-14 21:56:22 -0300

    add modal in post page

--- src/Views/post/index.tsx
+++ src/Views/post/index.tsx
@@ -1,10 +1,11 @@
-import { ArrowLeft, CalendarBlank, DotsThree, GlobeSimple, Mailbox, MapPin, Phone } from "@phosphor-icons/react";
+import { ArrowLeft, CalendarBlank, DotsThree, GlobeSimple, Mailbox, MapPin, Phone, X } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 import { Button } from "../../components/button";
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Post() {
     return (
-        <article className="w-full bg-slate-50 px-2 py-4 space-y-5">
+        <article className="w-full bg-slate-50 px-2 py-4 space-y-5 rounded-lg">
             <div className="w-full flex justify-between items-center text-zinc-600">
                 <Link to='/posts'>
                     <ArrowLeft size={20}/>
@@ -47,11 +48,58 @@ export function Post() {
                     <span className="flex gap-2 text-sm text-slate-800 font-normal"><Phone size={18} /> 462-***-**** x4542</span>
                     <span className="flex gap-2 text-sm text-slate-800 font-normal"><MapPin size={18} /> Rippin Parkway, Coral Springs 64554-****</span>
                 </div>
-                <div className="w-full flex justify-center">
-                    <Button size="default">
-                        Contratar
-                    </Button>
-                </div>
+                <Dialog.Root>
+                    <Dialog.Trigger>
+                        <div className="w-full flex justify-center">
+                            <Button size="default">
+                                Contratar
+                            </Button>
+                        </div>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay className="fixed inset-0 bg-slate-900/40 backdrop-blur"/>
+                        <Dialog.Content className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[600px] px-3 py-3 space-y-4 bg-slate-200 rounded-md">
+                            <div className="space-y-2">
+                                <Dialog.Title className="text-xl font-extrabold text-slate-950 leading-none">
+                                    Proposta
+                                </Dialog.Title>
+                                <Dialog.Description className="text-sm font-normal leading-normal text-slate-950">
+                                    Escreva uma proposta para o Fred
+                                </Dialog.Description>
+                            </div>
+
+                            <div className="h-0.5 w-full bg-black/10"/>
+
+                            <fieldset>
+                                <label 
+                                    htmlFor=""
+                                    className="text-sm font-normal leading-snug text-slate-700"
+                                >
+                                    Escreva uma mensagem?
+                                </label>
+                                <textarea 
+                                    name="" 
+                                    id=""
+                                    className="w-full min-h-[130px] px-3 py-3.5 rounded-md bg-slate-200 border border-slate-600/20 outline-none resize-none"
+                                    
+                                />
+                            </fieldset>
+                            <Dialog.Close asChild>
+                                <Button size="full">
+                                    Enviar
+                                </Button>
+                            </Dialog.Close>
+                            <Dialog.Close 
+                                className="absolute top-0 right-3 "
+                                aria-label="Close"
+                            >
+                                <X size={20} />
+                            </Dialog.Close>
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                </Dialog.Root>
+               
             </footer>
             
         </article> 
