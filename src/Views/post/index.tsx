f0ad60eb15f6a507fc47e1cbb27d5954fd915062
David Felipe <felipe30.10@outlook.com>
2024-08-14 21:19:15 -0300

    styled page

--- src/Views/post/index.tsx
+++ src/Views/post/index.tsx
@@ -4,18 +4,18 @@ import { Button } from "../../components/button";
 
 export function Post() {
     return (
-        <article className="w-full px-2 py-4 space-y-5">
-            <div>
+        <article className="w-full bg-slate-50 px-2 py-4 space-y-5">
+            <div className="w-full flex justify-between items-center text-zinc-600">
                 <Link to='/posts'>
-                    <ArrowLeft/>
+                    <ArrowLeft size={20}/>
                 </Link>
                 <button>
-                    <DotsThree/>
+                    <DotsThree size={20} weight="bold"/>
                 </button>
             </div>
-            <header>
-                <h1>Procura-se: Profissional de Limpeza Residencial com Experiência</h1>
-                <div className="flex gap-3">
+            <header className="w-full flex justify-between items-center">
+                <h1 className="text-xl font-extrabold text-slate-950 leading-none">Procura-se: Profissional de Limpeza Residencial com Experiência</h1>
+                <div className="flex gap-3 text-slate-800">
                     <div>
                         <CalendarBlank size={20} weight="fill"/>
                     </div>
@@ -25,10 +25,12 @@ export function Post() {
                 </div>
             </header>
             <main>
-                <p>
-                Estamos à procura de um(a) profissional de limpeza residencial dedicado(a) e confiável para cuidar de nossa casa em [Cidade Fictícia]. O(a) candidato(a) ideal deve ter experiência comprovada em limpeza profunda e organização de ambientes, além de um forte senso de responsabilidade e atenção aos detalhes. As tarefas incluem a limpeza de todos os cômodos, lavagem e passar roupas, e manutenção geral do lar. Oferecemos um ambiente de trabalho amigável e seguro, com horários flexíveis. Se você é uma pessoa detalhista e comprometida, esta é a oportunidade perfeita para você!
+                <p className="text-base font-normal leading-normal text-slate-950">
+                    Estamos à procura de um(a) profissional de limpeza residencial dedicado(a) e confiável para cuidar de nossa casa em [Cidade Fictícia]. O(a) candidato(a) ideal deve ter experiência comprovada em limpeza profunda e organização de ambientes, além de um forte senso de responsabilidade e atenção aos detalhes. As tarefas incluem a limpeza de todos os cômodos, lavagem e passar roupas, e manutenção geral do lar. Oferecemos um ambiente de trabalho amigável e seguro, com horários flexíveis. Se você é uma pessoa detalhista e comprometida, esta é a oportunidade perfeita para você!
                 </p>
-                <div className="flex gap-2 items-center justify-center">
+            </main>
+            <footer className="w-full flex flex-col gap-3">
+                <div className="flex gap-2">
                     <div className="w-8 h-w-8 overflow-hidden rounded-full">
                         <img 
                             src="http://github.com/PedroH2o.png"
@@ -40,16 +42,16 @@ export function Post() {
                         Pedro Yan
                     </span>
                 </div>
-            </main>
-            <footer>
-                <div>
-                    <span><Mailbox size={18}/> F****@hotmail.com</span>
-                    <span><Phone size={18}/> 462-***-**** x4542</span>
-                    <span><MapPin size={18}/> Rippin Parkway, Coral Springs 64554-****</span>
+                <div className="space-y-4">
+                    <span className="flex gap-2 text-sm text-slate-800 font-normal"><Mailbox size={18} /> F****@hotmail.com</span>
+                    <span className="flex gap-2 text-sm text-slate-800 font-normal"><Phone size={18} /> 462-***-**** x4542</span>
+                    <span className="flex gap-2 text-sm text-slate-800 font-normal"><MapPin size={18} /> Rippin Parkway, Coral Springs 64554-****</span>
                 </div>
-                <Button size="default">
-                    Contratar
-                </Button>
+                <div className="w-full flex justify-center">
+                    <Button size="default">
+                        Contratar
+                    </Button>
+                </div>
             </footer>
             
         </article> 
