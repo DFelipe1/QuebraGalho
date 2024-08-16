dc748d9f0aaace0262d494c95642975ee2cb49f3
David Felipe <felipe30.10@outlook.com>
2024-08-16 11:47:27 -0300

    add max width

--- src/Views/notification/index.tsx
+++ src/Views/notification/index.tsx
@@ -2,7 +2,7 @@ import { NotificationComponent } from "../../components/notificattionComponet";
 
 export function Notification() {
     return (
-        <div className="w-full px-2 py-4 space-y-5">
+        <div className="w-full max-w-4xl px-2 py-4 space-y-5">
           <NotificationComponent/>
           <NotificationComponent/>
           <NotificationComponent/>
