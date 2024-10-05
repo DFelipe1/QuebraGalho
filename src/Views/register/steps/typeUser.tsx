a48af9b2322a4aa3875539ba95c5bc1b3934c256
David Felipe <felipe30.10@outlook.com>
2024-10-05 15:52:49 -0300

    responsive page register

--- src/Views/register/steps/typeUser.tsx
+++ src/Views/register/steps/typeUser.tsx
@@ -1,5 +1,6 @@
 // import pana from "../../../assets/images/pana.png";
 // import bro from "../../../assets/images/bro.png";
+import { ArrowLeft, ArrowRight } from "@phosphor-icons/react";
 import { Button } from "../../../components/button";
 
 interface TypeUserProps {
@@ -12,12 +13,14 @@ export function TypeUser({setStep}: TypeUserProps) {
              <div className="w-full max-w-[600px] space-y-5">
                 <h1 className='w-full max-sm:text-4xl text-5xl text-center font-extrabold leading-none text-slate-950'>Como entrar na plataforma?</h1>
                 <span className=" w-full block text-xl text-center font-extrabold leading-snug text-slate-800">Escolha um tipo de categoria</span>
-                <div className="flex gap-3 justify-between items-center">
+                <div className="flex max-sm:flex-col flex-row gap-3 justify-between items-center">
                     <Button size="full" onClick={() => setStep('SearchService')}>
+                        <ArrowLeft size={20}/>
                         <span>Encontrar profissional</span>
                     </Button>
                     <Button size="full" variant="secondary" onClick={() => setStep('FormService')}>
                         <span>Prestar serviço</span>
+                        <ArrowRight size={20}/>
                     </Button>
                 </div>
             </div>
