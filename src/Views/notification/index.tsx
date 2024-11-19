2307f1c18ab3762bb0fb156b857cbaa4874f49a3
David Felipe <felipe30.10@outlook.com>
2024-11-18 21:32:41 -0300

    new notifications

--- src/Views/notification/index.tsx
+++ src/Views/notification/index.tsx
@@ -3,12 +3,9 @@ import { NotificationComponent } from "../../components/notificattionComponet";
 export function Notification() {
     return (
         <div className="w-full max-w-4xl px-2 py-4 space-y-5">
-          <NotificationComponent/>
-          <NotificationComponent/>
-          <NotificationComponent/>
-          <NotificationComponent/>
-          <NotificationComponent/>
-          <NotificationComponent/>
+          <NotificationComponent title="Confirme o contrato com o David" message="David mandou o contrato assine para aceitar" linkURL="/contract/1"/>
+          <NotificationComponent title="Mayk aceitou seu orçamento!!" message="Faça o contrato, para o trabalho com mayk." linkURL="/contract/"/>
+          <NotificationComponent title="Mayk te enviou uma proposta de trabalho" message="Converse com Mayk e acerte com ele todos os requisitos." linkURL="/chats/1"/>
         </div> 
     )
 }
--- src/components/notificattionComponet.tsx
+++ src/components/notificattionComponet.tsx
@@ -1,15 +1,21 @@
 import { Handshake, X } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 
-export function NotificationComponent() {
+interface NotificationComponentProps {
+    title: string
+    message: string
+    linkURL: string 
+}
+
+export function NotificationComponent({ title, message, linkURL}: NotificationComponentProps) {
     return (
-        <Link to="http://localhost:5173/chats/1" className="w-full px-3 py-5 rounded-lg flex gap-4 items-start bg-secondary-200">
+        <Link to={linkURL} className="w-full px-3 py-5 rounded-lg flex gap-4 items-start bg-secondary-200">
             <div className="w-fit h-fit p-4 bg-secondary-300 text-slate-900 rounded-lg">
                 <Handshake size={48} weight="fill"/>
             </div>
             <div className="w-full space-y-3">
-                <strong className="w-full text-lg font-extrabold text-slate-950 leading-snug"> Broklin quer contratar seus serviços! </strong>
-                <p className="w-full text-base text-slate-900 leading-normal">Lorem ipsum dolor sit amet consectetur. Facilisis nisl urna imperdiet suspendisse. Habitasse non id eu at consectetur.</p>
+                <strong className="w-full text-lg font-extrabold text-slate-950 leading-snug"> {title} </strong>
+                <p className="w-full text-base text-slate-900 leading-normal">{message}</p>
             </div>
             <button className="w-fit h-fit">
                 <X size={20}/>
