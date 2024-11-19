5d59ec49171ad93f74fb82885b6da37725807318
David Felipe <felipe30.10@outlook.com>
2024-11-18 21:30:13 -0300

    linked service to contract

--- src/Views/services/index.tsx
+++ src/Views/services/index.tsx
@@ -3,9 +3,9 @@ import { Service } from "./services";
 export function Services() {
     return (
         <div className="w-full max-w-4xl px-2 py-4 space-y-5">
-            <Service/>
-            <Service variant="process"/>
-            <Service variant="danger"/>
+            <Service contractURL="http://localhost:5173/contract/1"/>
+            <Service contractURL="http://localhost:5173/contract/1" variant="process"/>
+            <Service contractURL="http://localhost:5173/contract/1" variant="danger"/>
            
         </div> 
     )
--- src/Views/services/services.tsx
+++ src/Views/services/services.tsx
@@ -1,8 +1,9 @@
 import { Clock } from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
 import { tv, VariantProps } from "tailwind-variants";
 
 interface ServicesProps extends VariantProps<typeof serviceVariants>, VariantProps<typeof tagVariants> {
-
+    contractURL: string
 }
 
 const serviceVariants = tv({
@@ -38,9 +39,9 @@ const tagVariants = tv({
 
 })
 
-export function Service({ variant }: ServicesProps) {
+export function Service({ variant, contractURL }: ServicesProps) {
     return (
-        <div className={serviceVariants({ variant })}>
+        <Link to={contractURL} className={serviceVariants({ variant })}>
             <div className="w-full flex justify-between items-center">
                 <div className="flex gap-2 items-center justify-center">
                     <div className="w-8 h-w-8 overflow-hidden rounded-full">
@@ -69,6 +70,6 @@ export function Service({ variant }: ServicesProps) {
                     28/Jun/2024
                 </span>
             </div>
-        </div>
+        </Link>
     )
 }
