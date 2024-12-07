b3b326c78335cbd9a4c19ceac0e597090878a831
David Felipe <felipe30.10@outlook.com>
2024-12-07 10:28:24 -0300

    fix: resolve bug link redirect

--- src/Views/services/index.tsx
+++ src/Views/services/index.tsx
@@ -4,9 +4,9 @@ export function Services() {
     return (
         <div className="w-full max-w-4xl px-2 py-4 space-y-5">
             <h1 className="text-xl text-left text-slate-950 font-extrabold leading-none">Meus pedidos de serviço</h1>
-            <Service contractURL="http://localhost:5173/contract/1"/>
-            <Service contractURL="http://localhost:5173/contract/1" variant="process"/>
-            <Service contractURL="http://localhost:5173/contract/1" variant="danger"/>
+            <Service contractURL="/contract/1"/>
+            <Service contractURL="/contract/1" variant="process"/>
+            <Service contractURL="/contract/1" variant="danger"/>
            
         </div> 
     )
